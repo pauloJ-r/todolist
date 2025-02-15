e4aa095da3ec8ab16ae4d2ad319c02702cf6c3d9
pauloJ-r <pj74586@gmail.com>
2025-02-15 08:50:04 -0300

    feat: fazendo conexão de tasks com db

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize')
 const config = require('../config/database')
 const connection = new Sequelize(config)
+const Tasks = require('../models/Tasks')
+
+Tasks.init(connection)
 
 module.exports = connection
