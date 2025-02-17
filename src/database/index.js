b7b237ac4de77692835a2739cc817bdcebf13676
pauloJ-r <pj74586@gmail.com>
2025-02-17 00:28:49 -0300

    feat: fazendo conexão de tags

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize')
 const config = require('../config/database')
 const connection = new Sequelize(config)
 const Tasks = require('../models/Tasks')
+const Tags = require('../models/Tags')
 
+Tags.init(connection)
 Tasks.init(connection)
 
 module.exports = connection
