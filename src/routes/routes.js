7652eb4687b4ad9007baf6fa48c366aafc3c6ab5
pauloJ-r <pj74586@gmail.com>
2025-03-15 08:55:17 -0300

    feat : adicionado routes de tasks e tags

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const {Router} = require('express')
 const Tasks = require('../models/Tasks')
 const TasksController = require('../controller/TasksController')
 const TagsController = require('../controller/TagsController')
+const TasksTagsController = require('../controller/TasksTagsController')
 const routes = Router()
 
 // Rotas de tarefas
@@ -21,5 +22,16 @@ routes.post('/tags', TagsController.CreateTags);
 routes.put('/tags/:id',TagsController.UpdateTags);
 
 routes.delete('/tags/:id', TagsController.DeleteTags);
+
+// Rotas de tarefas por tag
+routes.get('/tasks/tags/all', TasksTagsController.getAllTasksWithTags);
+
+routes.get('/tasks/tags', TasksTagsController.filterTasksByTags);
+
+routes.get('/tasks/:tag_id', TasksTagsController.getTasksByTag);
+
+routes.post('/tasks/tags', TasksTagsController.createTaskTag);
+
+routes.delete('/tasks/tags/:id', TasksTagsController.removeTaskFromTag);
 
 module.exports = routes
