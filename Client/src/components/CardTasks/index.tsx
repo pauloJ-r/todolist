55300ef29c02c195b235ec59e0182e74c7da4bf2
pauloJ-r <pj74586@gmail.com>
2025-04-16 10:52:46 -0300

    feat: adicionando o background com base no que vem do backend

--- Client/src/components/CardTasks/index.tsx
+++ Client/src/components/CardTasks/index.tsx
@@ -2,25 +2,47 @@ import styles from "./styles.module.css";
 import trash from "../../assets/icons/trash.svg";
 import edit from "../../assets/icons/edit.svg";
 import { Link } from "react-router-dom";
+import { deleteTasks } from "../../services/TasksServices";
 import { ICardTasksProps } from "./types";
 
 export default function CardTasks({ tasks, onDelete }: ICardTasksProps) {
-    return (
-        <div className={styles.Cardtasks}>
+  return (
+    <div className={styles.Cardtasks}>
+      <div>
         <div>
-          <div>
-            <strong>{tasks.title}</strong>{" "}
-            {tasks.tags_name && <small>{tasks.tags_name}</small>}
-          </div>
-          {tasks.description && <span>{tasks.description}</span>}
-          {tasks.status && <span>{tasks.status}</span>}
+          <strong>{tasks.title}</strong>{" "}
+          {tasks.tags?.map((tag, index) => (
+            <small
+              key={index}
+              className={styles.tag}
+              style={{ backgroundColor: tag.color }}
+            >
+              {tag.name}
+            </small>
+          ))}
+
         </div>
-        <div>
-          <Link to={`/tasks/${tasks.id}`}>
-            <img src={edit} alt="Editar" />
-          </Link>
-          <img src={trash} alt="Excluir" onClick={() => onDelete(tasks)} />
-        </div>
+        {tasks.description && <span>{tasks.description}</span>}
+        {tasks.status && <span>{tasks.status}</span>}
       </div>
-    );
+      <div>
+        <Link to={`/tasks/${tasks.id}`}>
+          <img src={edit} alt="Editar" />
+        </Link>
+        <img
+          src={trash}
+          alt="Excluir"
+          onClick={async () => {
+            try {
+              await deleteTasks(tasks.id);
+              onDelete(tasks);
+            } catch (error) {
+              console.error("Erro ao deletar tarefa:", error);
+            }
+          }}
+        />
+
+      </div>
+    </div>
+  );
 }
