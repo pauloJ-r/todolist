bb9ef2cefd68670d0a3cbde8ae3aea8504b5c112
pauloJ-r <pj74586@gmail.com>
2025-04-16 10:52:15 -0300

    style: atualizando dados do type

--- Client/src/@types/Tags.ts
+++ Client/src/@types/Tags.ts
@@ -2,7 +2,6 @@ interface ITags {
     id: number;
     name: string;
     color: string;
-    description: string;
 }
 
 export type { ITags };
--- Client/src/@types/Tasks.ts
+++ Client/src/@types/Tasks.ts
@@ -1,12 +1,12 @@
+import { ITags } from "./Tags";
+
 interface ITask {
     id: number;
     title: string;
     status: string;
     priority: number;
     description: string;
-    tags_id: number;
-    tags_name: string;
-    tags_color: string;
+    tags: ITags[];
 }
 
 export type { ITask };
